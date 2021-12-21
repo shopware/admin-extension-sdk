2afab4a4c64c923f827a6ae9e791690d75f45ef6
Jannis Leifeld <j.leifeld@shopware.com>
2021-12-21 10:56:19 +0100

    Add locationId property to locationUpdateHeight event

--- src/location/index.ts
+++ src/location/index.ts
@@ -1,20 +1,31 @@
 import { send } from '../channel';
 
-// TODO: add documentation
-export const is = (location: string): boolean => {
+function getLocationId():string|null {
   const params = new URLSearchParams(window.location.search);
 
-  return params.get('location-id') === location;
+  return params.get('location-id');
+}
+
+// TODO: add documentation
+export const is = (location: string): boolean => {
+  return getLocationId() === location;
 }
 
 export const updateHeight = (height?: number): Promise<void|null> => {
   if (height) {
-    return send('locationUpdateHeight', { height });
+    return send('locationUpdateHeight', {
+      height,
+      locationId: getLocationId(),
+    });
   }
 
   // if no height is defined then send the current document height
   const currentHeight = document.documentElement.scrollHeight;
-  return send('locationUpdateHeight', { height: currentHeight });
+
+  return send('locationUpdateHeight', {
+    height: currentHeight,
+    locationId: getLocationId(),
+  });
 };
 
 // TODO: add startAutoResizer to update the height automatically
@@ -29,4 +40,9 @@ export type locationUpdateHeight = {
    * The height of the iFrame
    */
   height: number,
+
+  /**
+   * The locationID of the current element
+   */
+  locationId: string | null,
 }
