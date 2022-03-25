b10fad1455060ad32d23d2b35d466aa831702c7a
Sebastian Seggewiss <s.seggewiss@shopware.com>
2022-03-25 07:57:45 +0100

    Add basic entity names as type

--- src/data/repository.ts
+++ src/data/repository.ts
@@ -3,9 +3,10 @@ import type Criteria from './Criteria';
 import type { ApiContext } from './_internals/EntityCollection';
 import type EntityCollection from './_internals/EntityCollection';
 import type { Entity } from './_internals/Entity';
+import type { entityName } from '.';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export default (entityName: string) => ({
+export default (entityName: entityName) => ({
   search: (criteria: Criteria, context?: ApiContext): Promise<EntityCollection | null> => {
     return send('repositorySearch', { entityName, context, criteria });
   },
@@ -36,7 +37,7 @@ export default (entityName: string) => ({
 
 export type repositoryGet = {
   responseType: Entity | null,
-  entityName: string,
+  entityName: entityName,
   id: string,
   context?: ApiContext,
   criteria?: Criteria,
@@ -44,21 +45,21 @@ export type repositoryGet = {
 
 export type repositorySearch = {
   responseType: EntityCollection,
-  entityName: string,
+  entityName: entityName,
   criteria?: Criteria,
   context?: ApiContext,
 }
 
 export type repositorySave = {
   responseType: void,
-  entityName: string,
+  entityName: entityName,
   entity: Entity,
   context?: ApiContext,
 }
 
 export type repositoryClone = {
   responseType: unknown,
-  entityName: string,
+  entityName: entityName,
   entityId: string,
   context?: ApiContext,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -67,27 +68,27 @@ export type repositoryClone = {
 
 export type repositoryHasChanges = {
   responseType: boolean,
-  entityName: string,
+  entityName: entityName,
   entity: Entity,
 }
 
 export type repositorySaveAll = {
   responseType: void,
-  entityName: string,
+  entityName: entityName,
   entities: EntityCollection,
   context?: ApiContext,
 }
 
 export type repositoryDelete = {
   responseType: void,
-  entityName: string,
+  entityName: entityName,
   entityId: string,
   context?: ApiContext,
 }
 
 export type repositoryCreate = {
   responseType: Entity,
-  entityName: string,
+  entityName: entityName,
   entityId?: string,
   context?: ApiContext,
 }
