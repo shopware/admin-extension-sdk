20ede425303f5d0aaaf788368897288c31d51497
Sebastian Seggewiss <s.seggewiss@shopware.com>
2023-05-25 16:07:29 +0200

    Apply scd feedback

--- devenv.nix
+++ devenv.nix
@@ -2,5 +2,5 @@
 
 {
   languages.javascript.enable = lib.mkDefault true;
-  languages.javascript.package = lib.mkDefault pkgs.nodejs-16_x;
+  languages.javascript.package = lib.mkDefault pkgs.nodejs-18_x;
 }
